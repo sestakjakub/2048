a69eb530d5a3341d9a7ddb62cdf835761baafb37
Jakub Sestak <sestak.jakub@gmail.com>
2014-04-19 18:07:11 +0200

    design changes

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,10 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  var span = document.createElement("span");
+  span.textContent = tile.value;
+  inner.appendChild(span);
+
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
